4f7b4f0d8cb4c8d1ddf162f8ea52250c21d205eb
woojin0603 <kwi0707@gmail.com>
2026-07-13 23:48:28 +0900

    회원 도메인 개발 및 테스트까지(18강)

---
+++ 7. spring-jpa1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    // ManyToOne : 기본 fetch가 FetchType.EAGER
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // JPQL select o From order o; -> SQL select * from order (n + 1)
+
+    // OneToMany : 기본 fetch가 FetchType.LAZY
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;    // 주문 시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태: [ORDER, CANCEL]
+
+
+    // == 연관관계 메서드 == //
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    /*public static void main(String[] args) {
+        Member member = new Member();
+        Order order = new Order();
+
+        order.setMember(member);
+    }*/
+
+}
