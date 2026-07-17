5c240ccb4cc04b2cafd49cd3fc694809ed9e59b8
woojin0603 <kwi0707@gmail.com>
2026-07-17 15:18:35 +0900

    엔티티 매핑 학습(17강)

--- 8. spring-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 8. spring-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -39,12 +40,79 @@ public class JpaMain {
             em.persist(member1);
             em.persist(member2);
 */
+/*
             // 변경 감지
             Member member = em.find(Member.class, 150L);
             member.setName("ZZZZZ");
             //em.persist(member);   변경 후에 persist 쓰면 안됨(변경 감지로 영속성 컨텍스트가 자동으로 update 쿼리를 날려줌)
             System.out.println("====================================");
+*/
+/*
+            // flush
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
+            em.flush();
+            System.out.println("====================================");
+*/
+
+          /*  // 준영속 상태
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAA");
+
+            //em.detach(member);  // 준영속 상태로 전환(영속성 컨텍스트에서 분리)(위 경우에서는 분리되었으므로 update 쿼리가 안나감)
+            em.clear(); // 영속성 컨텍스트 자체를 초기화
+
+            Member member2 = em.find(Member.class, 150L);
+*/
+            /*// ORDINAL 예제
+            Member member = new Member();
+            member.setId(1L);
+            member.setUserName("A");
+            member.setRoleType(RoleType.USER);
+
+            Member member2 = new Member();
+            member2.setId(2L);
+            member2.setUserName("B");
+            member2.setRoleType(RoleType.ADMIN);
+
+            Member member3 = new Member();
+            member3.setId(3L);
+            member3.setUserName("C");
+            member3.setRoleType(RoleType.GUEST);
+
+            em.persist(member);
+            em.persist(member2);
+            em.persist(member3);*/
+
+            // 기본키 매핑
+           /* Member member = new Member();
+            member.setUserName("C");
+
+            System.out.println("===============================");
+            em.persist(member);
+            System.out.println("member.id = " + member.getId());
+            System.out.println("===============================");
+*/
+            Member member1 = new Member();
+            member1.setUserName("A");
+
+            Member member2 = new Member();
+            member1.setUserName("B");
+
+            Member member3 = new Member();
+            member1.setUserName("C");
+
+            System.out.println("===============================");
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+
+            System.out.println("===============================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- 8. spring-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 8. spring-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,25 +1,32 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+/*@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)*/
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",    // 매필할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Column(name = "name", updatable = false, nullable = false)
+    private String userName;
 
-    // JPA를 위함
-    public Member() {
-    }
+    private int age;
 
     public Long getId() {
         return id;
@@ -29,11 +36,107 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /* @Enumerated
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }*/
+
+    // JPA를 위함
+    public Member() {
+    }
+
+
 }
