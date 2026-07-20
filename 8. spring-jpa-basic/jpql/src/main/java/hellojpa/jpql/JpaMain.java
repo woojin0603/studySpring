488ea85a5865d6b9f5cbc7e5d9b872e3bd11d55b
woojin0603 <kwi0707@gmail.com>
2026-07-20 20:48:30 +0900

    경로 표현식, 페치조인(기본)(51강)

--- 8. spring-jpa-basic/jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ 8. spring-jpa-basic/jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa.jpql;
 
 import jakarta.persistence.*;
 
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -14,7 +15,53 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+           // String query = "select m From Member m join fetch m.team";
+            String query = "select t From Team t join fetch t.members m";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .getResultList();
+
+            System.out.println("result = " + result.size());
+
+          /*  for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("- member = " + member);
+                }
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+
+                // 회원 100명 -> N + 1
+            }*/
+
+            /*Team team = new Team();
             team.setName("teamA");
             em.persist(team);
 
@@ -35,7 +82,7 @@ public class JpaMain {
                     .getResultList();
             for (Integer s : result) {
                 System.out.println("s = " + s);
-            }
+            }*/
 
             /*String query = "select nullif(m.username, '관리자') as username from Member m";
             List<String> resultList = em.createQuery(query, String.class)
@@ -68,8 +115,6 @@ public class JpaMain {
                 System.out.println("objects = " + objects[1]);
                 System.out.println("objects = " + objects[2]);
             }*/
-
-
 
             tx.commit();
         } catch (Exception e) {
