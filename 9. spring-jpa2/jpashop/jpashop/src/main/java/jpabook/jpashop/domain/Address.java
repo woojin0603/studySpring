93cad97c06ba8166bafdbf6b0597811bc5ad2a2c
woojin0603 <kwi0707@gmail.com>
2026-07-21 23:12:52 +0900

    spring-jpa2 프로젝트 생성, 회원 등록 API 학습(5강)

---
+++ 9. spring-jpa2/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // JPA 스펙상 만들어둔 기본 생성자
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
---
+++ 9. spring-jpa2/jpashop/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+    }
+
+}
