9a8c44da386d3606b36eb99b43f15726427fa087
woojin0603 <kwi0707@gmail.com>
2026-07-26 16:36:29 +0900

    JPA 공통 인터페이스 기능 학습(11강)

--- 10. spring-datajpa/data-jpa/data-jpa/src/main/java/study/data_jpa/repository/MemberRepository.java
+++ 10. spring-datajpa/data-jpa/data-jpa/src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package study.data_jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String name);   // 구현하지 않아도 query method 기능을 자동으로 써서 동작함.
 
 }
--- 10. spring-datajpa/data-jpa/data-jpa/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ 10. spring-datajpa/data-jpa/data-jpa/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -29,7 +31,36 @@ class MemberJpaRepositoryTest {
 
         // 같은 트랜잭션 안에서는 영속성 컨텍스트의 동일성을 보장해준다(true 반환)
         assertThat(findMember).isEqualTo(member);
+    }
 
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증 테스트
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증 테스트
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증 테스트
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증 테스트
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        
+        // 멤버 2개 데이터 삭제 후 전체 카운트 검증
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 
 }
