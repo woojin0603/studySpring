8b104971946445c619f7f5df2dd6d7a87738ccd8
woojin0603 <kwi0707@gmail.com>
2026-08-02 23:22:53 +0900

    프록시 팩토리, 어드바이저, 포인트컷 학습(73강)

---
+++ 12. spring-advanced/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,24 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.TargetSource;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args);
+        Object result = invocation.proceed();   // 알아서 target을 찾아서 파라미터를 넘겨줌
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
