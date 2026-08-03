de3c3d96e7191494d046a59870ac943f136fd299
woojin0603 <kwi0707@gmail.com>
2026-08-03 11:27:22 +0900

    Aspect 프록시 학습(85강)

---
+++ 12. spring-advanced/proxy/src/main/java/hello/proxy/config/v6_aop/AopConfig.java
@@ -0,0 +1,17 @@
+package hello.proxy.config.v6_aop;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v6_aop.aspect.LogTraceAspect;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@Import({AppV1Config.class, AppV2Config.class})
+public class AopConfig {
+
+    @Bean
+    public LogTraceAspect logTraceAspect(LogTrace logTrace) {
+        return new LogTraceAspect(logTrace);
+    }
+}
