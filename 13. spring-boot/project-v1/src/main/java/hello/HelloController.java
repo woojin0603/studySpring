c65d7bc734baaa2de8c8d731e3f380275c297887
woojin0603 <kwi0707@gmail.com>
2026-08-07 21:29:02 +0900

    스프링 자동 구성 라이브러리 사용 및 동작 학습(49강)

---
+++ 13. spring-boot/project-v1/src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ 13. spring-boot/project-v2/src/main/java/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
