441a93208112c5d0369ed6544f08f729ea7b1132
woojin0603 <kwi0707@gmail.com>
2026-08-07 19:22:23 +0900

    순수 라이브러리 예제 개발 및 학습(41강)

---
+++ 13. spring-boot/memory-v1/src/test/java/memory/MemoryFinderTest.java
@@ -0,0 +1,19 @@
+package memory;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        System.out.println("memory = " + memory);
+        assertThat(memory).isNotNull();
+    }
+}
